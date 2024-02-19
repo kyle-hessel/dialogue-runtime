0adaeb95da7a7b7c84cb91adedecdd943a29e48b
PollutedMind <pollutedmind@proton.me>
2024-02-19 01:21:19 -0700

    Textbox instantiation using scene resources inside of playwright_dialogue_manager's _ready function was added.

--- src/playwright_dialogue_manager.cpp
+++ src/playwright_dialogue_manager.cpp
@@ -1,13 +1,24 @@
 #include "playwright_dialogue_manager.h"
+
+#include "playwright_textbox.h"
+#include "playwright_textbox_response.h"
+
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/classes/engine.hpp>
+#include <godot_cpp/classes/resource_loader.hpp>
+#include <godot_cpp/classes/packed_scene.hpp>
 
 #include <godot_cpp/variant/utility_functions.hpp>
 
 using namespace godot;
 
 void PlaywrightDialogueManager::_bind_methods() {
+	
 
+
+	ADD_SIGNAL(MethodInfo("dialogue_complete"));
+	ADD_SIGNAL(MethodInfo("dialogue_trigger"));
+	ADD_SIGNAL(MethodInfo("end_branch"));
 }
 
 PlaywrightDialogueManager::PlaywrightDialogueManager() {
@@ -24,5 +35,13 @@ void PlaywrightDialogueManager::_ready() {
 	}
 	else {
 		UtilityFunctions::print("is this thing on?");
+
+		ResourceLoader* re_lo = ResourceLoader::get_singleton();
+
+		textbox_scene = re_lo->load("res://addons/playwright_runtime/scenes/playwright_textbox.tscn");
+		textbox_response_scene = re_lo->load("res://addons/playwright_runtime/scenes/playwright_textbox_response.tscn");
+
+		textbox_inst = Object::cast_to<PlaywrightTextbox>(textbox_scene->instantiate());
+		textbox_response_inst = Object::cast_to<PlaywrightTextboxResponse>(textbox_response_scene->instantiate());
 	}
 }
--- src/playwright_dialogue_manager.h
+++ src/playwright_dialogue_manager.h
@@ -2,6 +2,10 @@
 #define PLAYWRIGHT_DIALOGUE_MANAGER_H
 
 #include <godot_cpp/classes/control.hpp>
+#include <godot_cpp/classes/packed_scene.hpp>
+
+#include "playwright_textbox.h"
+#include "playwright_textbox_response.h"
 
 namespace godot {
 
@@ -10,6 +14,12 @@ namespace godot {
 
 		private:
 			int branch_index;
+
+			Ref<PackedScene> textbox_scene;
+			Ref<PackedScene> textbox_response_scene;
+
+			PlaywrightTextbox* textbox_inst;
+			PlaywrightTextboxResponse* textbox_response_inst;
 				
 		protected:
 			static void _bind_methods();
